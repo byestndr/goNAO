31cd63967fdef81780bdbfb80ab1bb1a60954544
byestndr <andrew.k.pham@outlook.com>
2025-01-31 20:38:02 -0600

    Initial Commit

---
+++ naoai.py
@@ -0,0 +1,178 @@
+import speech_recognition as sr
+from requests import post
+from re import sub
+from re import DOTALL
+import qi
+import sys
+import time
+import argparse
+import traceback
+import os
+from configparser import ConfigParser
+from ollama import chat
+from ollama import ChatResponse
+
+class audiorecorder():
+    def __init__(self, app):
+        app.start()
+        session = app.session
+        # Connect to the services
+        try:
+            self.aas = session.service("ALAudioRecorder")
+            self.tts= session.service("ALTextToSpeech")
+            print("Connected to ALAudioRecorder and ALTextToSpeech service")
+        except Exception as e:
+            print("Could not connect to service")
+            traceback.print_exc()
+            sys.exit(1)
+    def startRecord(self, filename, filetype, samplerate, channels):
+        self.aas.startMicrophonesRecording(filename, filetype, samplerate, channels)
+    def stopRecord(self):
+        self.aas.stopMicrophonesRecording()
+    def speechTalk(self, reply):
+        self.tts.setLanguage("English")
+        self.tts.say(reply)
+
+    
+
+# This connects to the NAO
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", type=str, default="127.0.0.1",
+                        help="IP address for the NAO robot. Cannot be a simulated robot as they are not supported")
+    parser.add_argument("--port", type=int, default=9559,
+                        help="NAO port")
+    parser.add_argument("--model", choices=['gemini', 'deepseek', "gemma"], default="gemma", 
+                        help="Choose an AI model for NAO to use")
+    parser.add_argument("--norobot", "-n", action='store_true', default=False, 
+                        help="Runs the script without connecting to the NAO robot")
+
+    args = parser.parse_args()
+    if args.norobot == False:
+        try:
+            # Initialize qi framework.
+            connection_url = "tcp://" + args.ip + ":" + str(args.port)
+            ipadd = args.ip
+            app = qi.Application(["NAOAI", "--qi-url=" + connection_url])
+        except RuntimeError:
+            print ("Can't connect to NAO at \"" + args.ip + "\" at port " + str(args.port) +".\n"
+                   "Please check your script arguments. Run with -h option for help.")
+            sys.exit(1)
+
+
+
+# AI response class
+class airesponse():
+    def gemini(self, prompt):
+        if args.model == "gemini":
+            # Sets config variables
+            scriptpath = (os.path.dirname(os.path.realpath(__file__)))
+            configpath = scriptpath + "/config.ini"
+            config = ConfigParser()
+
+            # Get Gemini API key if it doesn't exist
+            if os.path.isfile(configpath) == True:
+                config.read(configpath)
+                api_key = config.get('Main', 'api_key')
+            else:
+                keysave = input("Set a Gemini API key: ")
+                config['Main'] = {'api_key': keysave}
+                with open(configpath, 'w') as configfile:
+                    config.write(configfile)
+                api_key = config.get('Main', 'api_key')
+
+            # Sends the data to Gemini
+            url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-exp:generateContent?key={api_key}"
+            data = {
+                "system_instruction": {
+                    "parts": 
+                        {"text": "You are a robot named 'nao' responding to a human in a conversation"}
+                },
+                "contents": [{
+                "parts":[{"text": prompt}]
+                }],
+                "safetySettings": [
+                {
+                    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                    "threshold": "BLOCK_ONLY_HIGH"
+                },
+                {
+                    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+                }
+                ],
+               }
+            response = post(url, headers={'Content-Type': 'application/json'}, json=data)
+            # This is a debugging thingy
+            # print(response)
+            # This prints the result from Gemini
+            # It converts the json into actual text and then regex's all the * out
+            x = response.json()
+            content = (x["candidates"][0]["content"]["parts"][0]["text"]) 
+            print(sub('[*]', " ", content))
+            return content
+    def ollamaresponse(self, prompt):
+        if args.model == "deepseek":
+            model = 'deepseek-r1:1.5b'
+        else:
+            model = 'gemma2:2b'
+        
+        response: ChatResponse = chat(model=model, messages=[
+          {
+            'role': 'user',
+            'content': prompt,
+          },
+        ])
+        
+        if args.model == "deepseek":
+            response = response['message']['content']
+            nothink = sub(r"<think>.*?</think>\n?", "", response, flags=DOTALL) 
+            print(nothink)
+            return(nothink)
+        else:
+            response = response['message']['content']
+            print(sub('[*]', " ", response))
+            return(sub('[*]', " ", response))
+
+# Checks if the no robot flag is on and runs depending on if it is
+if args.norobot == False:
+    try: 
+        # Loops the querying and responds
+        while True:
+            start_record = audiorecorder(app)
+            channels = [0, 0, 1, 0]
+            start_record.stopRecord()
+            start_record.startRecord("/home/nao/recordings/microphones/request.wav", "wav", 48000, channels)
+            time.sleep(5)
+            start_record.stopRecord()
+            # SCPs the file over to the host
+            sshcom = f"nao@{ipadd}:/home/nao/recordings/microphones/request.wav /mnt/c/Users/Student/Documents/NAOAI/request.wav"    
+            os.system("sshpass -p 'nao' scp -o StrictHostKeyChecking=no "+sshcom) 
+            # Reads file
+            audfile = "/mnt/c/Users/Student/Documents/NAOAI/request.wav"
+            sp = sr.Recognizer()
+            with sr.AudioFile(audfile) as sourceaud:
+                    audio = sp.record(sourceaud)
+            # Transcribes
+            try:
+                prompt = sp.recognize_sphinx(audio)
+                print(prompt)
+            except sr.UnknownValueError:
+                print("Sphinx could not understand audio")
+            except sr.RequestError as e:
+                print("Sphinx error; {0}".format(e))
+
+            # Make NAO say the response
+            start_record.speechTalk(sub('[*]', " ", airesponse(prompt)))
+            time.sleep(1)
+
+
+    except KeyboardInterrupt:
+        print("Exiting the program")
+        time.sleep(1)
+        print("Stopping current recordings")
+        start_record.stopRecord()
+        print("Stopped")
+        sys.exit()
+else:
+    airesponse().ollamaresponse(input("Enter the prompt: "))
