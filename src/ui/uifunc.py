4a0c925d6fd6ee864c1cc44fa2c0931c09f8255c
Andrew Pham <andrew.k.pham@outlook.com>
2025-06-04 18:23:23 -0500

    Add comments for my sanity

--- src/ui/uifunc.py
+++ src/ui/uifunc.py
@@ -6,7 +6,6 @@ from PySide6 import QtWidgets
 from ui.window import Ui_MainWindow
 from resource.config import Configuration as config
 import walkingnao.walk as walk
-import resource.qiapi as qiapi
 import walkingnao.autowalk as autowalk
 from naoai import naoai
 from walkingnao import buttonpresses
@@ -48,22 +47,29 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.stop.clear()
 
         if self.checkBox.isChecked() is False:
+            
             buttonDetector = threading.Thread(
                 target=buttonpresses.JoyButton().controllerButtons,
                 args=(ip, port, self.model, started, qistart, walkMode, self.checkBox.isChecked(), self.apikey, self.stop))
+
             naoTranscribeOff = threading.Thread(
                 target=buttonpresses.JoyButton().onAiOff,
                 args=(ip, port, self.model, started, qistart, self.apikey, self.sysprompt, self.stop, self.logQueue))
+
             walker = threading.Thread(
                 target=walk.ConnectionDetails.runFromMain,
                 args=(ip, port, qistart, walkMode, self.stop, self.logQueue))
+
         elif self.checkBox.isChecked() is True:
+
             autotalk = threading.Thread(
                 target=naoai.ConnectionDetails.runFromMainStart,
                 args=(ip, port, self.model, qistart, self.checkBox.isChecked(), self.apikey, self.stop, self.logQueue))
+
             walker = threading.Thread(
                 target=autowalk.ConnectionDetails.runFromMain,
                 args=(ip, port, qistart, self.stop))
+
         self.appendLog("Starting walker...")
         walker.start()
         if args.auto is False:
@@ -82,42 +88,59 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         return
 
     def disconnectPushed(self):
+        """ Runs when disconnect button is pressed. Sets stop variable to true. """
         self.appendLog("Disconnecting...")
         self.stop.set()
 
     def config(self):
         """ Set variable values and return one if a value is not found """
+
+        ### GEMINI SECTION
+        # Checks if a model is selected
         if self.gemini_button.isChecked() is False and self.ollama_button.isChecked() is False:
             self.appendLog("Please select between Ollama or Gemini")
             return 1
 
+        # If Gemini is selected, it sets some variables
         if self.gemini_button.isChecked() is True:
             self.model = "gemini"
             self.apikey = self.api_key.text()
 
+        # Checks if the API box is filled and returns if not
         if self.gemini_button.isChecked() is True and self.apikey == "":
             self.appendLog("Enter a Gemini API key to use gemini")
             return 1
 
+        # Save the API key to config if filled
         if self.gemini_button.isChecked() is True and self.apikey:
             config().geminiSaveKeyGUI(self.apikey)
-        if self.ollama_button.isChecked() is True:           
+
+        ### OLLAMA SECTION
+        # If Ollama is selected, it sets some variables
+        if self.ollama_button.isChecked() is True:      
             modelInput = self.lineEdit.text()
             self.model = config().modelType(modelInput)
+
+        # Checks if the model is entered and returns if not
         if self.ollama_button.isChecked() is True and self.model == "":
             self.appendLog("Please enter an ollama model to use.")
             return 1
 
+        # If the model value returned equals 1 then it returns with no model found
         if self.model == 1:
             self.appendLog("No models found, download some models or use Gemini")
             return 1
+
         elif 'available' in self.model:
             self.appendLog("The model was not found. Make sure it is spelled right and if you've also typed its tag.")
             self.appendLog(self.model)
             return 1
+
+        # If Ollama is checked and the model is correct, it saves it
         if self.ollama_button.isChecked() is True and self.model:
             config().modelSaveGUI(modelInput)
 
+        # Checks if the system prompt is set
         if self.sys_prompt.text() == "":
             self.appendLog("Please enter a system prompt.")
             return 1
